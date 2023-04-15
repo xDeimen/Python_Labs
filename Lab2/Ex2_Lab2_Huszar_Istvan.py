fdd850cb9e7c4a6c168b327112acdcc3b56e7089
Huszar Istvan <44548937+xDeimen@users.noreply.github.com>
2023-04-15 21:17:48 +0300

    Update Ex2_Lab2_Huszar_Istvan.py
    Added detailed description of the function

--- Lab2/Ex2_Lab2_Huszar_Istvan.py
+++ Lab2/Ex2_Lab2_Huszar_Istvan.py
@@ -1,3 +1,15 @@
+'''
+Am definit variabila cost_cazare ca fiind prețul pe noapte al hotelului (140 lei) înmulțit cu numărul de nopți de cazare.
+Apoi, am folosit o declarație if-elif pentru a stabili prețul transportului în funcție de locația aleasă.
+Am definit variabila cost_transport ca fiind prețul specific pentru locația respectivă.
+Următorul pas a fost de a calcula costul închirierii mașinii.
+Am definit variabila cost_inchiriere_masina ca fiind prețul pe zi al închirierii mașinii (40 lei) înmulțit cu numărul de nopți de cazare.
+Am definit variabila cost_masa ca fiind suma pe care dorești să o cheltuiești pentru masă.
+În final, am calculat costul total al călătoriei adunând toate costurile definite anterior.
+Funcția returnează costul total calculat.
+'''
+
+
 def cost_calatorie(destinatie, nopti_cazare):
     cost_cazare = 140 * nopti_cazare
     if destinatie == "Cluj":
