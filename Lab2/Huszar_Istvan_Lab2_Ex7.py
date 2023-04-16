038870cbc8cf72ba6861b08e9dd0e7fa943989b4
Huszar Istvan <44548937+xDeimen@users.noreply.github.com>
2023-04-16 11:45:26 +0300

    Ex7_Lab2

---
+++ Lab2/Huszar_Istvan_Lab2_Ex7.py
@@ -0,0 +1,22 @@
+
+x = int(input("x= "))
+y = int(input("y= "))
+# Ce lipsește din următorul cod pentru a afișa "Hello, world!" dacă x este 10 sau y este 20?
+print(" Ce lipsește din următorul cod pentru a afișa 'Hello, world!' dacă x este 10 sau y este 20?")
+if x == 10 or y == 20:
+    print("Hello, world!")
+
+#Ce lipsește din următorul cod pentru a afișa "Hello, world!" dacă x este 10 și y este 20?
+print("Ce lipsește din următorul cod pentru a afișa 'Hello, world!' dacă x este 10 și y este 20?")
+if x == 10 and y == 20:
+    print("Hello, world!")
+
+#Ce lipsește din următorul cod pentru a afișa "Hello, world!" dacă x nu este 10?
+print("Ce lipsește din următorul cod pentru a afișa 'Hello, world!' dacă x nu este 10?")
+if not x == 10:
+    print("Hello, world!")
+
+#Ce lipsește din următorul cod pentru a afișa "Hello, world!" dacă x este mai mare sau egal cu 10 și mai mic decât 20?
+print("Ce lipsește din următorul cod pentru a afișa 'Hello, world!' dacă x este mai mare sau egal cu 10 și mai mic decât 20?")
+if 10 <= x < 20:
+    print("Hello, world!")
