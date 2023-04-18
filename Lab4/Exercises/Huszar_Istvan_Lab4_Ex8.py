43ead67ebf6d32af08226a1104af12ee7b0efe4b
Huszar Istvan <44548937+xDeimen@users.noreply.github.com>
2023-04-18 12:48:24 +0300

    Ex8_Lab4

---
+++ Lab4/Exercises/Huszar_Istvan_Lab4_Ex8.py
@@ -0,0 +1,8 @@
+"""
+Pentru a elimina toate aparitiile literei "X" din sirul de caractere dat putem utiliza functia replace()
+pentru a inlocui fiecare aparitie cu un sir gol, ca mai jos
+"""
+
+A = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+A = A.replace("X", "")
+print(A)
